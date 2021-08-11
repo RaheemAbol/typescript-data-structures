ecd0208c2a4ae5e7d4c192843e1bd539fdf55869
Raheem Abolfathzadeh <raheemabol91@gmail.com>
2021-08-10 21:08:43 -0400

    v1.0  added solution to doubly linked list problem via vanilla javascript

--- index.js
+++ index.js
@@ -1,28 +1,65 @@
-var helloWorld = "hello world";
-console.log(helloWorld);
-var baseBall = {
-    name: "Reds",
-    numOfPlayers: 14,
-    city: "columbus"
-};
-var basketBall = {
-    name: "Bucks",
-    numOfPlayers: 12,
-    city: "florida"
-};
-console.log(baseBall.city);
-
-
 /*
- Here is your task for the rest of today:
-Create a repo for yourself named “typescript-data-structures” or something like that.
-In the repo start a new npm project. Add typescript and nodemon as project dependencies.
-Create npm watch scripts for tsc / nodemon as we discussed.
-Write a “Node” interface for a doubly linked list. It should:
-Get or point to the next Node in the list. 
-Get or point to the previous Node in the list. 
-Indicate if the Node is at the:
-Head (beginning) of the list.
-Tail (end) of the list. 
-Whatever else you need to make it work.
-*/
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+class DoublyLinkedList {
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    add(val){
+        var newNode = new Node(val);
+        // if head is null we set the head and tail to be the node
+        if(this.length === 0){
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+        // setting tail.next to the new tail
+            this.tail.next = newNode;
+        // setting the newNode.prev to be the old tail
+            newNode.prev = this.tail;
+            // updating this.tail to be the new node
+            this.tail = newNode;
+        }
+        //adding one to the length and then returning the list
+        this.length++;
+        return this;
+    }
+    find(index){
+        // edge case making sure we don't go out of bounds
+        if(index < 0 || index >= this.length) return null;
+         var count, current;
+        if(index <= this.length/2){
+            count = 0;
+            current = this.head;
+        // while count is not equal to index we will traverse the list and add one to count
+            while(count !== index){
+                current = current.next;
+                count++;
+            }
+        } else {
+            // to traverse backwards we start from the end by setting the count to the last index in the list and current to the tail
+            count = this.length - 1;
+            current = this.tail;
+            while(count !== index){
+            // while count is not equal to index we will set current to the previous index and subtract one
+                current = current.prev;
+                count--;
+            }
+        }
+        return current;
+    }
+}
+
+var list = new DoublyLinkedList()
+list.add("Head")
+list.add("Middle")
+list.add("Tail")
+console.log(list.find(2))
+
+*/ 
--- index.ts
+++ index.ts
@@ -1,25 +1,65 @@
-let helloWorld = "hello world";
-console.log(helloWorld);
-
-
-
-interface Team {
-    name: string;
-    numOfPlayers: number;
-    city: string;
+/*
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+class DoublyLinkedList {
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    add(val){
+        var newNode = new Node(val);
+        // if head is null we set the head and tail to be the node
+        if(this.length === 0){
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+        // setting tail.next to the new tail
+            this.tail.next = newNode;
+        // setting the newNode.prev to be the old tail    
+            newNode.prev = this.tail;
+            // updating this.tail to be the new node
+            this.tail = newNode;
+        }
+        //adding one to the length and then returning the list
+        this.length++;
+        return this;
+    } 
+    find(index){
+        // edge case making sure we don't go out of bounds
+        if(index < 0 || index >= this.length) return null;
+         var count, current;
+        if(index <= this.length/2){
+            count = 0;
+            current = this.head;
+        // while count is not equal to index we will traverse the list and add one to count
+            while(count !== index){
+                current = current.next;
+                count++;
+            }
+        } else {
+            // to traverse backwards we start from the end by setting the count to the last index in the list and current to the tail
+            count = this.length - 1;
+            current = this.tail;
+            while(count !== index){
+            // while count is not equal to index we will set current to the previous index and subtract one
+                current = current.prev;
+                count--;
+            }
+        }
+        return current;
+    }
 }
 
+var list = new DoublyLinkedList()
+list.add("Head")
+list.add("Middle")
+list.add("Tail")
+console.log(list.find(2))
 
-const baseBall: Team = {
-   name: "Reds",
-   numOfPlayers: 14,
-   city: "columbus"
-}
-
-const basketBall: Team = {
-    name: "Bucks",
-    numOfPlayers: 12,
-    city: "florida"
-}
-
-console.log(basketBall.name);
+*/
